b860ac3f250e800fb9a5b7b216a3e970607223fa
Ethan Skowronski-Lutz <33399972+ethanmsl@users.noreply.github.com>
2023-02-03 20:06:44 -0800

    docs: doctest, crate description, docstring formatting exploration

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+//! # Tiny Applause
+//! This crate is just to trial using the CLAP crate (for creating CLI Apps)
+//!
+//! ## Other goals:
+//! - test docstring formatting
+//! - test doctests
+//! - test proper structuring
+//! - test multi-target binary builds
+//! - test graphical output options
+//!
+
+/// First line is a short summary describing function.
+///
+/// The next lines present detailed documentation. Code blocks start with
+/// triple backquotes and have implicit `fn main()` inside
+/// and `extern crate <cratename>`. Assume we're testing `doccomments` crate:
+///
+/// ```
+/// let result = tinyapplause::add(2, 3);
+/// assert_eq!(result, 5);
+/// ```
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
